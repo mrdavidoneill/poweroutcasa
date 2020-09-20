87133995345ea64ba06c0a09f44e9ed5fc7812aa
David ONeill <mrdavidoneillll@gmail.com>
2020-09-20 12:20:42 +0200

    Change from print statements to logger - console logger set at debug level - log file set at info level -- only records when alert is sent Added mark_delete_msg method - Adds flag to email for deletion Added expunge on exit - Removes all emails marked for deletion

---
+++ app/logger.py
@@ -0,0 +1,26 @@
+import logging
+import logging.handlers
+
+######### DEBUG LEVELS ##########
+CONSOLE_LEVEL = logging.DEBUG
+FILE_LEVEL = logging.INFO
+#################################
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Log to console
+ch = logging.StreamHandler()
+ch.setLevel(CONSOLE_LEVEL)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# Log to file
+fh = logging.handlers.RotatingFileHandler(
+    'emailchecker.log', maxBytes=1024*2, backupCount=1)
+fh.setLevel(FILE_LEVEL)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
--- app/readmail.py
+++ app/readmail.py
@@ -5,6 +5,11 @@ import email
 import time
 from email.mime.text import MIMEText
 from datetime import datetime
+from .logger import logger
+
+########## CHECK TIME DELAY ##########
+DELAY_MINS = 15
+######################################
 
 
 class EmailChecker:
@@ -23,30 +28,35 @@ class EmailChecker:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        print("Closing...")
+        logger.debug("Expunging...")
+        self.mailbox.expunge()
+        logger.debug("Closing...")
         self.mailbox.close()
-        print("Mailbox closed")
+        logger.debug("Mailbox closed")
         self.mailbox.logout()
-        print("Mailbox logged out")
+        logger.debug("Mailbox logged out")
 
     def login(self):
         try:
+            logger.debug("Logging in...")
             self.mailbox = imaplib.IMAP4_SSL(self.__server)
             self.mailbox.login(self.__from_address, self.__pwd)
             self.mailbox.select('inbox')
+            logger.debug("Logged in...")
             return True
         except Exception, e:
-            print str(e)
+            logger.critical(str(e))
             return False
 
     def get_unseen_router_mail(self, expected_from, expected_subject):
         """ Retrieves mail from mailbox """
         mail = []
-        typ, data = self.mailbox.search(None, "UNSEEN")
+        self.mailbox.recent()
+        typ, data = self.mailbox.search(None, "ALL")
         mail_ids = data[0]
         id_list = mail_ids.split()
 
-        for i in reversed(id_list[-2:-1]):
+        for i in reversed(id_list):
             typ, data = self.mailbox.fetch(i, '(RFC822)')
 
             for response_part in data:
@@ -58,35 +68,44 @@ class EmailChecker:
                     date = email.utils.parsedate(date)
 
                     if email_subject != expected_subject:
+                        logger.debug('Ignoring...')
+                        logger.debug('\tFrom : ' + email_from)
                         continue
                     if email_from != expected_from:
+                        logger.debug('Ignoring...' + '\n')
+                        logger.debug('\tSubject : ' + email_subject)
                         continue
 
-                    print 'From : ' + email_from + '\n'
-                    print 'Subject : ' + email_subject + '\n'
-                    # print 'Date : ' + date + '\n'
-                    print(date)
+                    logger.debug('From : ' + email_from)
+                    logger.debug('Subject : ' + email_subject)
+                    logger.debug(datetime.fromtimestamp(time.mktime(date)))
 
                     mail.append(time.mktime(date))
+
+            self.mark_delete_msg(i)
+
         return mail
+
+    def mark_delete_msg(self, msg_id):
+        """ Marks single email message for deletion """
+        self.mailbox.store(msg_id, "+FLAGS", "\\Deleted")
 
     def is_ok(self, expected_from, expected_subject):
         """ Return true if has received OK email in last 12 minutes """
         for date in self.get_unseen_router_mail(expected_from, expected_subject):
-            twelve_min = 1000 * 60 * 12
+            delay_seconds = 60 * DELAY_MINS
             now = time.time()
-            print(date)
-            print(now)
-            print((now - date) <= twelve_min)
-            print(ms_to_min(now - date))
-            if (now - date) <= twelve_min:
+            logger.debug(
+                "Ok received {} minutes ago".format(s_to_min(now - date)))
+            if (now - date) <= delay_seconds:
                 return True
+        logger.debug("Not OK")
         return False
 
     def send_alert(self):
         """ Send email alert """
         try:
-            print("*** Sending ALERT ***")
+            logger.info("*** Sending ALERT ***")
             msg = MIMEText("ALERT!!! POWER OUT!!!", "plain")
             msg['Subject'] = 'Power Outage in casa!'
             msg['From'] = self.__from_address
@@ -97,21 +116,26 @@ class EmailChecker:
             server.login(self.__from_address, self.__pwd)
             server.sendmail(self.__from_address, [
                 self.__to_address], msg.as_string())
-            print("*** ALERT sent ***")
+            logger.debug("*** ALERT sent ***")
         except Exception, e:
-            print(e)
+            logger.critical(e)
         finally:
             if server:
+                logger.debug("Server quitting...")
                 server.quit()
+                logger.debug("Server quit successful")
+            else:
+                logger.debug("No server to quit")
 
 
 def print_all_dict(dictionary):
+    """ Helper to print all dictionary K,V pairs.
+        One per line printed to console """
     for k, v in dictionary.items():
         print("{}: {}".format(k, v))
 
 
-def ms_to_min(ms):
-    """ Convert milliseconds to minutes 
-        Return float
-    """
-    return float(ms / 1000.0 / 60.0)
+def s_to_min(s):
+    """ Convert milliseconds to minutes
+        Return float """
+    return round(float(s / 60.0), 1)
